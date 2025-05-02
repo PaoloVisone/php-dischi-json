082d9023d994bfe0f03cb3549506927f1846ec06
PaoloVisone <polvis.pv.22@gmail.com>
2025-05-02 21:44:26 +0200

    Update JSON cover URLs and add server script for album submission

---
+++ server.php
@@ -0,0 +1,36 @@
+<?php
+
+// Recupero il file json
+$file_json = file_get_contents('./dischi.json');
+// echo $file_json;
+
+// Decodifico il JSON in un array PHP
+$dischi = json_decode($file_json);
+
+// Ricavo i dati del form tramite POST
+$titolo = $_POST['titolo'] ?? '';
+$artista = $_POST['artista'] ?? '';
+$url = $_POST['url'] ?? '';
+$anno = $_POST['anno'] ?? '';
+$genere = $_POST['genere'] ?? '';
+
+// Aggiungo il nuovo disco all'array
+$nuovoDisco = [
+    "titolo" => $titolo,
+    "artista" => $artista,
+    "url della cover" => $url,
+    "anno di pubblicazione" => $anno,
+    "genere" => $genere
+];
+
+$dischi[] = $nuovoDisco;
+
+// Codifico l'array aggiornato in JSON
+$file_json_aggiornato = json_encode($dischi);
+// var_dump($file_json_aggiornato);
+
+// Scrivo i dati aggiornati nel file JSON
+file_put_contents("./dischi.json", $file_json_aggiornato);
+
+// Reindirizzo l'utente alla pagina principale
+header('Location: index.php');
